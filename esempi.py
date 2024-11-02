544e9e614eaede27475f3d8336b4d5e78fb2834a
lors10 <lors.93.10@gmail.com>
2024-11-02 17:35:12 +0100

    prova di push

--- esempi.py
+++ esempi.py
@@ -1,8 +1,18 @@
-x = int(input())
-y = int(input())
-
-x = x / y
-y = y / x
-
-print(x)
-print(y) 
+#
+#try:
+	# It's a place where
+	# you can do something 
+    # without asking for permission.
+#except:
+	# It's a spot dedicated to 
+    # solemnly begging for forgiveness.
+#
+#except ZeroDivisionError | ValueError | TypeError | AttributeError | SyntaxError
+#
+try:
+    value = int(input('Enter a natural number: '))
+    print('The reciprocal of', value, 'is', 1/value)
+except (ValueError, ZeroDivisionError, TypeError, AttributeError, SyntaxError):
+    print('I do not know what to do.')
+except:
+    print('Division by zero is not allowed in our Universe')
